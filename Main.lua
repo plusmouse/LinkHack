675108733d640b4b4c04acaa3c9f747e81f7879c
plusmouse <plusmouse@protonmail.com>
2024-03-09 22:08:35 +0000

    Initial commit

---
+++ Main.lua
@@ -0,0 +1,29 @@
+local function GenerateTooltip(itemID, keyLevel, itemLevel, upgradeLevel)
+  local tooltipData = C_TooltipInfo.GetItemKey(itemID, itemLevel, 0)
+  tooltipData.lines[1].leftColor = ITEM_QUALITY_COLORS[Enum.ItemQuality.Epic].color
+  table.insert(tooltipData.lines, 2, {
+    type = 0,
+    leftText = PLAYER_DIFFICULTY_MYTHIC_PLUS .. " " .. keyLevel,
+    leftColor = GREEN_FONT_COLOR,
+  })
+  table.insert(tooltipData.lines, 4, {
+    type = 0,
+    leftText = ITEM_UPGRADE_TOOLTIP_FORMAT:format(upgradeLevel, 8),
+    leftColor = NORMAL_FONT_COLOR,
+  })
+  for index, line in ipairs(tooltipData.lines) do
+    if line.leftText == AUCTION_HOUSE_BUCKET_VARIATION_EQUIPMENT_TOOLTIP then
+      table.remove(tooltipData.lines, index)
+      table.remove(tooltipData.lines, index - 1)
+      break
+    end
+  end
+  local info = {
+    tooltipData = tooltipData,
+  }
+  GameTooltip:ProcessInfo(info)
+  GameTooltip:Show()
+end
+
+GameTooltip:SetOwner(UIParent, "ANCHOR_CENTER")
+GenerateTooltip(211470, 13, 460, 3)
